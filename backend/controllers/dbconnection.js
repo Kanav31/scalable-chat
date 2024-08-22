59bde2f0265dc9023fb8c15c27f8dba9293924f7
Kanav Agarwal <agarwalkanav3108@gmail.com>
2024-08-22 21:05:39 +0530

    new update

--- backend/controllers/dbconnection.js
+++ backend/controllers/dbconnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://kanav:kanav123@cluster0.fxgpn.mongodb.net/scalable-chat');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);
